ccdfc967ba73c3e83c2d8a5165d43c699e54609f
Daan Bergmans <daan.bergmans@student.pxl.be>
2017-05-01 20:48:52 +0200

    Added wordcounter

---
+++ Wordcounter/1_Windows/CounterWindow.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Wordcounter._2_Services;
+
+namespace Wordcounter
+{
+    public partial class MainWindow : Window
+    {
+
+        private static CounterService counterService;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void txtText_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            counterService = new CounterService();
+            counterService.Text = txtText.Text;
+            counterService.Lines = txtText.LineCount;
+
+            lblCharacters.Content = counterService.countCharacters();
+            lblWords.Content = counterService.countWords();
+            lblLines.Content = counterService.countLines();
+        }
+
+        private void muRefresh_Click(object sender, RoutedEventArgs e)
+        {
+            counterService = new CounterService();
+
+            txtText.Clear();
+            lblCharacters.Content = "";
+            lblWords.Content = "";
+            lblLines.Content = "";
+        }
+    }
+}
---
+++ Wordcounter/2_Services/CounterService.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Wordcounter._2_Services
+{
+    public class CounterService
+    {
+
+        private string text;
+        private int lines;
+
+        public CounterService() {
+            text = "";
+            lines = 0;
+        }
+
+        public string Text
+        {
+            get { return text; }
+            set { text = value; }
+        }
+
+        public int Lines
+        {
+            get { return lines; }
+            set { lines = value; }
+        }
+
+        public int countWords()
+        {
+            return text.Split(new char[] { ' ', '.', ':' }, StringSplitOptions.RemoveEmptyEntries).Count();
+        }
+
+        public int countCharacters()
+        {
+            return text.Count();
+        }
+
+        public int countLines()
+        {
+            return lines;
+        }
+
+    }
+}
